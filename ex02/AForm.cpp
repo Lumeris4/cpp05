0af62252f4cf5e6a15160973e8046c007d30f3b7
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-03-05 10:05:44 +0100

    Ajout de Aform

---
+++ ex02/AForm.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 14:10:05 by lelanglo          #+#    #+#             */
+/*   Updated: 2025/03/05 10:01:12 by lelanglo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+
+AForm::AForm(): _name("undifined"), _is_signed(false), _grade_sign(0), _grade_excecute(0)
+{
+	return;
+}
+
+AForm::AForm(std::string name, int sign, int exec): _name(name), _grade_sign(sign), _grade_excecute(exec)
+{
+	return;
+}
+
+AForm::~AForm() {};
+
+AForm::AForm(AForm &copy): _name(copy._name), _is_signed(false), _grade_sign(copy._grade_sign), 
+						_grade_excecute(copy._grade_excecute) {};
+
+AForm &AForm::operator=(const AForm &other)
+{
+	if (this != &other) {}
+	return *this;
+}
+
+int AForm::get_exec() const
+{
+	return this->_grade_excecute;
+}
+
+int AForm::get_sign() const
+{
+	return this->_grade_sign;
+}
+
+const std::string AForm::get_name() const
+{
+	return this->_name;
+}
+
+bool AForm::get_is_signed() const
+{
+	return this->_is_signed;
+}
+
+const char *AForm::GradeTooHighException::what() const throw()
+{
+	return "Grade too high";
+}
+
+const char *AForm::GradeTooLowException::what() const throw()
+{
+	return "Grade too low";
+}
+
+std::ostream &operator<<(std::ostream &os, const AForm &b)
+{
+	os << b.get_name() << " is a Form with the grade " << b.get_is_signed()
+	<< " required for signed and " << b.get_exec() << " required for execute and actually the Form is ";
+	if (b.get_is_signed() == false)
+		std::cout << " not signed\n";
+	else
+		std::cout << " signed\n";
+	return os;
+}
+
+bool AForm::be_signed(Bureaucrat &bureau)
+{
+	if (bureau.Get_Grade() >= this->get_is_signed())
+	{
+		this->_is_signed = true;
+		return true;
+	}
+	else
+	{
+		throw GradeTooLowException();
+		return false;
+	}
+}
---
+++ ex02/Bureaucrat.cpp
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 15:57:28 by lelanglo          #+#    #+#             */
+/*   Updated: 2025/03/05 10:02:03 by lelanglo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+
+Bureaucrat::Bureaucrat(): _name("undifined"), _grade(0)
+{
+	std::cout << "Constructor called\n";
+}
+
+Bureaucrat::Bureaucrat(int grade, std::string name): _name(name)
+{
+	std::cout << this->_name << " Constructor called\n";
+	if (grade > 150)
+		throw GradeTooHighException();
+	else if (grade < 1)
+		throw GradeTooLowException();
+	this->_grade = grade; 
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << this->_name << " destructor called\n";
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+{
+	*this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	this->_grade = other._grade;
+	return *this;
+}
+
+int Bureaucrat::Get_Grade() const
+{
+	return this->_grade;
+}
+
+std::string Bureaucrat::Get_Name() const
+{
+	return this->_name;
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Error grade too low";
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Error grade too High";
+}
+
+void Bureaucrat::promote()
+{
+	if (this->_grade + 1 > 150)
+		throw GradeTooHighException();
+	else
+		this->_grade++;
+}
+
+void Bureaucrat::demote()
+{
+	if (this->_grade - 1 < 1)
+		throw GradeTooHighException();
+	else
+		this->_grade--;
+}
+
+void Bureaucrat::signForm(AForm &lala)
+{
+	try
+	{
+		if (lala.be_signed(*this) == true)
+			std::cout << this->_name << " signed " << lala.get_name();
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << this->_name << " couldn't sign " << lala.get_name()
+					<< " Because " << e.what() << std::endl;
+	}
+				
+}
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &b)
+{
+	os << b.Get_Name() << " is a bureaucrat with the grade " << b.Get_Grade();
+	return os;
+}
